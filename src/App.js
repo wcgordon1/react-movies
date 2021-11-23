8dc9c30083f09cce9a84614d9d3f6ca88de445e8
wcgordon1 <wcgordon1@gmail.com>
2021-11-23 11:42:49 -0800

    setting up so far has been pretty easy.Time to see how I do with the JavaScript and API

--- src/App.js
+++ src/App.js
@@ -8,8 +8,17 @@ const api = {
 
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    return `Today is ${day}, ${month} ${date}, ${year}.`
+  }
   return (
-    <div className="App">
+    <div className="app">
       <main>
         <div className="search-box">
           <input
@@ -17,6 +26,18 @@ function App() {
             className="search-bar"
             placeholder="Search..."
           />
+        </div>
+        <div>
+          <div className='location-box'>
+            <div className='location'>New York City, US</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
+          <div className='weather-box'>
+            <div className='temp'>
+              15 F
+            </div>
+            <div className='weather'>Sunny</div>
+          </div>
         </div>
       </main>
     </div>
